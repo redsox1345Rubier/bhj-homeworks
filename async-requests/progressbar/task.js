b79fedd449f84f74d77a5b66a6af6e21b0a86b0f
redsox1345Rubier <genki999@mail.ru>
2023-06-05 00:40:05 +0300

    first commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,16 @@
+let form = document.getElementById('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    
+    const progress = document.getElementById( 'progress' );
+    const formData = new FormData(form)
+
+    xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable)
+            progress.value = e.loaded / e.total;
+    }
+    
+    xhr.open('POST', form.action, true);
+    xhr.send(formData);
+})
